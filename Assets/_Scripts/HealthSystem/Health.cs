10a49d39e7589f8b96027336135ed97b56b47829
murmoh <mursal.mohamoud1@gmail.com>
2023-04-30 20:34:35 -0400

    add zombie animation/prefabs

---
+++ Assets/_Scripts/HealthSystem/Health.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
